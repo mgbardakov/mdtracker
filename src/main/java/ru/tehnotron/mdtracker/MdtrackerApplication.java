017f499e82dc0a86c4ee2d20d65f209f92cf58be
Maxim Bardakov <mgbardakov@gmail.com>
2021-06-28 12:26:06 +0300

    initial project

---
+++ src/main/java/ru/tehnotron/mdtracker/MdtrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.tehnotron.mdtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tehnotron/mdtracker/MdtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tehnotron.mdtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

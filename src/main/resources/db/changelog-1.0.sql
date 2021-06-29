80cd146268b4407e078d54dc3092d96e4ec71f3d
mgbardakov <mgbardakov@gmail.com>
2021-06-29 14:03:22 +0300

    liquibase added, temporary disabled security. Closes #2

--- src/main/resources/db/changelog-1.0.sql
+++ src/main/resources/db/changelog-1.0.sql
@@ -1,27 +1,22 @@
-DROP TABLE IF EXISTS position CASCADE;
-DROP TABLE IF EXISTS employee CASCADE;
-DROP TABLE IF EXISTS device CASCADE;
-DROP TABLE IF EXISTS device_movement CASCADE;
-
-CREATE TABLE position (
+CREATE TABLE IF NOT EXISTS position (
     id SERIAL PRIMARY KEY,
     name TEXT
 );
 
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
     id SERIAL PRIMARY KEY,
     name TEXT,
     position_id INT REFERENCES position(id)
 );
 
-CREATE TABLE device (
+CREATE TABLE IF NOT EXISTS device (
     id SERIAL PRIMARY KEY,
     name TEXT,
     verification_expire TIMESTAMP,
     taken BOOLEAN
 );
 
-CREATE TABLE device_movement (
+CREATE TABLE IF NOT EXISTS device_movement (
     id SERIAL PRIMARY KEY,
     taken TIMESTAMP,
     returned TIMESTAMP,
